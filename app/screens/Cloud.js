914718e3ccb15739c59508d7904bc8f6613b1fef
Rujal Shrestha <rujal4shrz@gmail.com>
2020-11-22 14:54:44 +0545

    Upload local images to backend

--- app/screens/Cloud.js
+++ app/screens/Cloud.js
@@ -1,10 +1,85 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import CameraRoll from '@react-native-community/cameraroll';
+import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+
+import stl from '../assets/styles/style';
+import theme from '../assets/theme';
+
+import {uploadImage} from '../../utils/calls';
+
+const uploadLocalImages = async (setShowFindBtn) => {
+  try {
+    const cameralRollRes = await CameraRoll.getPhotos({
+      first: 9999,
+      assetType: 'Photos',
+    });
+
+    const images = cameralRollRes.edges.map((item) => item.node.image);
+
+    const formData = new FormData();
+    images.slice(0, 3).forEach((image) => {
+      const filename = image.uri.split('/').pop();
+      formData.append('image', {
+        uri: image.uri,
+        type: 'image/jpeg',
+        name: filename,
+      });
+    });
+
+    const res = await uploadImage(formData);
+    console.log('RES: ', res);
+    setShowFindBtn(false);
+  } catch (err) {
+    console.log('ERROR UPLOADING IMAGES: ', err);
+  }
+};
 
 export default function () {
+  const [showFindBtn, setShowFindBtn] = React.useState(true);
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const findSimilar = async () => {
+    await uploadLocalImages(setShowFindBtn);
+  };
+
   return (
-    <View>
-      <Text>Other screen</Text>
+    <View style={stl.flex}>
+      {showFindBtn ? (
+        <View style={style.buttonContainer}>
+          <Image
+            source={require('../assets/images/similarity_icon.png')}
+            style={style.icon}
+          />
+          <TouchableOpacity
+            style={stl.button}
+            activeOpacity={0.9}
+            onPress={() => findSimilar()}>
+            <Text style={style.btnText}>
+              {isLoading ? 'Processing...' : 'Click to find similar images'}
+            </Text>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <View><Text>Images</Text></View>
+      )}
     </View>
   );
 }
+
+const style = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    // backgroundColor: theme.colors.greenBg,
+  },
+  btnText: {
+    // color: theme.colors.greenBg,
+    fontSize: theme.sizes.font,
+  },
+  icon: {
+    width: 150,
+    height: 150,
+    marginBottom: theme.sizes.margin,
+  },
+});
--- utils/calls.js
+++ utils/calls.js
@@ -4,22 +4,39 @@ const call = async ({path, body, method = 'GET', token, headers = {}}) => {
   const res = await fetch(`${consts.HOST_URL}${path}`, {
     method,
     headers: {
-      'Content-Type': 'application/json',
       ...headers,
       Authorization: token ? `Bearer ${token}` : undefined,
     },
-    body: JSON.stringify(body),
+    body,
   });
 
   return await res.json();
 };
 
 export const login = (email, password) =>
-  call({path: '/users/login', body: {email, password}, method: 'POST'});
+  call({
+    path: '/users/login',
+    body: JSON.stringify({email, password}),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
 export const signup = (name, email, password, passwordConfirm) =>
   call({
     path: '/users/signup',
-    body: {name, email, password, passwordConfirm},
+    body: JSON.stringify({name, email, password, passwordConfirm}),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+export const uploadImage = (payload) => {
+  return call({
+    path: '/images/upload',
+    body: payload,
     method: 'POST',
   });
+};
